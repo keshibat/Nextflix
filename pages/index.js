6222c3414e30b546ccd7ccb681a2c601e1257e28
keshibat <kensukeshibata@gmail.com>
2022-01-06 09:18:47 +0900

    setting img src at card component

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head"
 import styles from "../styles/Home.module.css"
 import Banner from "../components/banner/banner";
 import NavBar from "../components/nav/navbar";
-
+import Card from "../components/card/card";
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -15,6 +15,9 @@ export default function Home() {
               subTitle="test test subtitles"
               imgUrl="/static/clifford.webp"
       />
+      <Card imgUrl="/static/clifford.webp" size="large" />
+      <Card imgUrl="/static/clifford.webp" size="medium" />
+      <Card imgUrl="/static/clifford.webp" size="small" />
     </div>
   )
 }
