f6f49774498a056342060d1dbade38d9788f3708
keshibat <kensukeshibata@gmail.com>
2022-02-15 14:40:55 +0900

    hooking up hasura url

---
+++ pages/api/login.js
@@ -0,0 +1,39 @@
+import { magicAdmin } from "../../lib/magic"
+import jwt from "jsonwebtoken"
+
+export default async function Login(req, res) {
+  if ( req.method === "POST") {
+    try {
+      const auth = req.headers.authorization
+      const didToken = auth ? auth.substr(7) : ""
+      console.log({didToken})
+
+      const metadata = await magicAdmin.users.getMetadataByToken(didToken)
+      console.log({metadata})
+
+      // create jwt
+
+      const token = jwt.sign({
+        ...metadata,
+        iat: Math.floor(Date.now() / 1000),
+        exp: Math.floor(Date.now() / 1000 +7 *24 *60 *60 ),
+          "https://hasura.io/jwt/claims": {
+          "x-hasura-allowed-roles": ["user", "admin"],
+          "x-hasura-default-role": "user",
+          "x-hasura-user-id": `${metadata.issuer}`,
+          },
+        },
+          "keepstaticlikewoolfabric12345678"
+      )
+
+      console.log({ token })
+
+      res.send({ done: true })
+    } catch(error) {
+      console.error({error})
+      res.status(500).send({ done: false})
+    }
+  } else {
+    res.send({ done: false })
+  }
+}
