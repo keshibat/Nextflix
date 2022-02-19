04a6edb1531759ea17b0ee0cd2191084f02ea536
keshibat <kensukeshibata@gmail.com>
2022-02-19 15:59:21 +0900

    login API JWT

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -12,7 +12,9 @@ const NavBar = () => {
 
   useEffect(async() => {
     try {
-      const { email } = await magic.user.getMetadata()
+      const { email, issuer } = await magic.user.getMetadata()
+      const didToken = magic.user.getIdToken()
+      console.log( { didToken })
       if(email) {
         setUsername(email)
       }
--- pages/api/login.js
+++ pages/api/login.js
@@ -1,11 +1,14 @@
+import { magicAdmin } from "../../lib/magic"
+
 export default async function login(req, res) {
     if(req.method === "POST") {
       try {
         const auth = req.headers.authorization;
-        const token = auth ? auth.substr(7) : "";
-        console.log({token});
-        console.log({auth});
-        res.send({ done: true });
+        const didToken = auth ? auth.substr(7) : "";
+        res.send({ didToken });
+
+        const metadata = await magicAdmin.users.getMetadataByToken(didToken);
+        console.log({ metadata });
       } catch {
         console.error("Something went wrong loggin in")
         res.status(500).send({ done: false });
