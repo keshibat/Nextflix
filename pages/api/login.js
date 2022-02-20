36442af88e99cf293544aeb7e730570dfd6129eb
keshibat <kensukeshibata@gmail.com>
2022-02-20 15:36:30 +0900

    ftb

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,14 +1,32 @@
 import { magicAdmin } from "../../lib/magic"
+import jwt from "jsonwebtoken"
 
 export default async function login(req, res) {
     if(req.method === "POST") {
       try {
         const auth = req.headers.authorization;
         const didToken = auth ? auth.substr(7) : "";
-        res.send({ didToken });
+        //invoke magic
 
         const metadata = await magicAdmin.users.getMetadataByToken(didToken);
         console.log({ metadata });
+        res.send({ done: true });
+
+        // create jwt
+        const token = jwt.sign({
+            ...metadata,
+            iat: Math.floor(Date.now() /1000),
+            exp: Math.floor(Date.now() /1000 +7 *24 * 60 * 60),
+            "https://hasura.io/jwt/claims": {
+              "x-hasura-allowed-roles": ["user", "admin"],
+              "x-hasura-default-role": "user",
+              "x-hasura-user-id": `${metadata.issuer}`,
+            },
+        },
+        "abcdeabcdeabcdeabcdeabcdeabcdeabc"
+        );
+        console.log({ token });
+
       } catch {
         console.error("Something went wrong loggin in")
         res.status(500).send({ done: false });
