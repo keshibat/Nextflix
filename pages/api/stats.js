9ec7051663989c63b7b1040b174f53e8054d4cb1
keshibat <kensukeshibata@gmail.com>
2022-03-14 14:42:29 +0900

    stats API wrap

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -7,29 +7,31 @@ export default async function stats(req, res) {
       if(!token) {
         res.status(403).send({})
       } else {
-        const videoId = req.query.videoId;
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
-        const userId = decodedToken.issuer;
-        const doesStatsExist = await findVideoIdByUser(userId, videoId);
-        if(doesStatsExist) {
-          // update it
-          const response = await udateStats(token, {
-            watched: true,
-            userId,
-            videoId,
-            favourited: 0,
-          });
-          res.send({ msg: "it works", updateStats: response})
-        } else {
-          // add it
-          const response = await udateStats(token, {
-            watched: true,
-            userId,
-            videoId,
-            favourited: 0,
-          });
-          res.send({ msg: "it works", updateStats: response})
+        const {videoId, favourited, watched = true } = req.body;
+        if (videoId) {
+          const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+          const userId = decodedToken.issuer;
+          const doesStatsExist = await findVideoIdByUser(userId, videoId);
+          if(doesStatsExist) {
+            // update it
+            const response = await udateStats(token, {
+              watched,
+              userId,
+              videoId,
+              favourited,
+            });
+            res.send({ data: response} )
+          } else {
+            // add it
+            const response = await udateStats(token, {
+              watched,
+              userId,
+              videoId,
+              favourited,
+            });
+            res.send({ data: response })
         }
+      }
       }
     } catch(error) {
       console.log("Error occured /stats", error)
