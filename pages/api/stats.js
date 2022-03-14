3dc4a545a5e68768d8608c65b1155ee8d02928fb
keshibat <kensukeshibata@gmail.com>
2022-03-14 10:28:01 +0900

    FindUserById GreaphQL

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,11 +1,23 @@
+import jwt from "jsonwebtoken"
+import { findVideoIdByUser } from '../../lib/db/hasura';
 export default async function stats(req, res) {
   if(req.method === "POST") {
     console.log({ cookies: req.cookies })
     try {
-      if(!req.cookies.token) {
+      const token = req.cookies.token;
+      if(!token) {
         res.status(403).send({})
       } else {
-        res.send({ msg: "it works"})
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        console.log({ decoded })
+
+        const userId = "did:ethr:0xf6682264c8eCC53aAA341da423582D8865F14240";
+        const videoId="0U0L4uT0btQ";
+
+        const findVideoId = await findVideoIdByUser(userId, videoId);
+        console.log({findVideoId})
+
+        res.send({ msg: "it works", decoded, findVideoId})
       }
     } catch(error) {
       console.log("Error occured /stats", error)
