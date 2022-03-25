3311c4a735642b3f9deb786a3ba17aa7d66d127c
keshibat <kensukeshibata@gmail.com>
2022-03-25 10:58:17 +0900

    ftb

--- pages/index.js
+++ pages/index.js
@@ -19,21 +19,35 @@ export async function getServerSideProps(context) {
 
   const disneyVideos = await getVideos("disney%20trailer");
   const productivityVideos = await getVideos("Productivity");
+
   const travelVideos = await getVideos("travel");
+
   const popularVideos = await getPopularVideos();
-  // const popularVideos = await getVideos("disney trailer");
   return {
-     props: { disneyVideos,  productivityVideos, travelVideos, popularVideos, watchItAgainVideos }
-  }
+     props: {
+       disneyVideos,
+       productivityVideos,
+       travelVideos,
+       popularVideos,
+       watchItAgainVideos,
+    },
+  };
 }
 
-export default function Home({disneyVideos, productivityVideos, travelVideos, popularVideos, watchItAgainVideos=[] }) {
+export default function Home({
+  disneyVideos,
+  productivityVideos,
+  travelVideos,
+  popularVideos,
+  watchItAgainVideos,
+ }) {
   return (
     <div className={styles.container}>
       <Head>
         <title>Nextflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <div className={styles.main}>
         <NavBar username="kenery"/>
         <Banner
@@ -42,6 +56,7 @@ export default function Home({disneyVideos, productivityVideos, travelVideos, po
                 subTitle="test test subtitles"
                 imgUrl="/static/clifford.webp"
         />
+
         <div className={styles.sectionWrapper}>
           <SectionCards title="Disney" videos={disneyVideos} size="large" />
           <SectionCards title="Watch it again" videos={watchItAgainVideos} size="small" />
